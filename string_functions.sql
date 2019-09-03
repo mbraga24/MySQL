dcea84799bb4e971142a78d06bc0bb9226197df6
Marlon Braga <rdg.brg@gmail.com>
2019-09-02 23:04:37 -0400

    CHAR_LENGTH

--- string_functions.sql
+++ string_functions.sql
@@ -145,3 +145,26 @@ SELECT CONCAT(title, ' was released in ', released_year) AS "Book's release year
 -- | Oblivion: Stories was released in 2004                                   |
 -- | Consider the Lobster was released in 2005                                |
 -- +--------------------------------------------------------------------------+
+
+SELECT title AS 'Title', CHAR_LENGTH(title) AS 'Character Count' FROM books;
+
+-- +-----------------------------------------------------+-----------------+
+-- | Title                                               | Character Count |
+-- +-----------------------------------------------------+-----------------+
+-- | The Namesake                                        |              12 |
+-- | Norse Mythology                                     |              15 |
+-- | American Gods                                       |              13 |
+-- | Interpreter of Maladies                             |              23 |
+-- | A Hologram for the King: A Novel                    |              32 |
+-- | The Circle                                          |              10 |
+-- | The Amazing Adventures of Kavalier & Clay           |              41 |
+-- | Just Kids                                           |               9 |
+-- | A Heartbreaking Work of Staggering Genius           |              41 |
+-- | Coraline                                            |               8 |
+-- | What We Talk About When We Talk About Love: Stories |              51 |
+-- | Where I'm Calling From: Selected Stories            |              40 |
+-- | White Noise                                         |              11 |
+-- | Cannery Row                                         |              11 |
+-- | Oblivion: Stories                                   |              17 |
+-- | Consider the Lobster                                |              20 |
+-- +-----------------------------------------------------+-----------------+
