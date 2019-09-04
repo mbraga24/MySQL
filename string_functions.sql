e8c58b5b7f53b1185673b0afbee697489b8e54fc
Marlon Braga <rdg.brg@gmail.com>
2019-09-04 12:54:06 -0400

    CONCAT/SUBSTRING

--- string_functions.sql
+++ string_functions.sql
@@ -182,3 +182,30 @@ FROM books;
 -- | Oblivion: Stories                                   |              17 |
 -- | Consider the Lobster                                |              20 |
 -- +-----------------------------------------------------+-----------------+
+
+SELECT 
+	CONCAT(SUBSTRING(title, 1, 10), '...') AS 'Short Title',
+	CONCAT(author_lname, ', ', author_fname) AS 'Author',
+	CONCAT(stock_quantity, ' in stock') AS 'Quantity'
+FROM books;
+
+-- +---------------+-----------------------+--------------+
+-- | Short Title   | Author                | Quantity     |
+-- +---------------+-----------------------+--------------+
+-- | The Namesa... | Lahiri, Jhumpa        | 32 in stock  |
+-- | Norse Myth... | Gaiman, Neil          | 43 in stock  |
+-- | American G... | Gaiman, Neil          | 12 in stock  |
+-- | Interprete... | Lahiri, Jhumpa        | 97 in stock  |
+-- | A Hologram... | Eggers, Dave          | 154 in stock |
+-- | The Circle... | Eggers, Dave          | 26 in stock  |
+-- | The Amazin... | Chabon, Michael       | 68 in stock  |
+-- | Just Kids...  | Smith, Patti          | 55 in stock  |
+-- | A Heartbre... | Eggers, Dave          | 104 in stock |
+-- | Coraline...   | Gaiman, Neil          | 100 in stock |
+-- | What We Ta... | Carver, Raymond       | 23 in stock  |
+-- | Where I'm ... | Carver, Raymond       | 12 in stock  |
+-- | White Nois... | DeLillo, Don          | 49 in stock  |
+-- | Cannery Ro... | Steinbeck, John       | 95 in stock  |
+-- | Oblivion: ... | Foster Wallace, David | 172 in stock |
+-- | Consider t... | Foster Wallace, David | 92 in stock  |
+-- +---------------+-----------------------+--------------+
